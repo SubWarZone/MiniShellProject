f184d956fd3b6f9aca3d3887b7f2342be4550edf
SubWarZone <hugo@debian.(none)>
2012-12-04 11:59:40 +0100

    main bien indenter

--- main.c
+++ main.c
@@ -5,77 +5,78 @@
 #define tailleBuffer 100
 
 int compterMots(char * chaine){
-       int i=1;
-       int compteur = 0;
-       //nous 
-       while(chaine[i] != '\0'){
-               if(chaine[i] == ' ' && chaine[i-1] != ' '){        
-                               compteur++;
-               }
-               i++;
-       }
-       return compteur + 1; // il y a compteur espace et donc compter+1 mots
+	int i=1;
+	int compteur = 0;
+	//nous 
+	while(chaine[i] != '\0'){
+		if(chaine[i] == ' ' && chaine[i-1] != ' '){        
+			compteur++;
+		}
+		i++;
+	}
+	return compteur + 1; // il y a compteur espace et donc compter+1 mots
 }
 
 char **separerLaChaine(char *chaine){
-       
+
 }
 
 int runcmd(char *cmd)
 {
- pid_t child_pid;
- int child_status;
+	pid_t child_pid;
+	int child_status;
 
- child_pid = fork();
- if(child_pid == 0) {
-   /* This is done by the child process. */
+	child_pid = fork();
+	if(child_pid == 0) {
+		/* This is done by the child process. */
 
-execvp(argv[0], argv);
-   
-   /* If execvp returns, it must have failed. */
+		execvp(argv[0], argv);
 
-   printf("Unknown command\n");
-   exit(0);
- }
- else {
-    /* This is run by the parent.  Wait for the child
-       to terminate. */
+		/* If execvp returns, it must have failed. */
 
-    do {
-      pid_t tpid = wait(&child_status);
-      if(tpid != child_pid) process_terminated(tpid);
-    } while(tpid != child_pid);
+		printf("Unknown command\n");
+		exit(0);
+	}
+	else {
+		/* This is run by the parent.  Wait for the child
+		to terminate. */
 
-    return child_status;
- }
+		do {
+			pid_t tpid = wait(&child_status);
+			if(tpid != child_pid) process_terminated(tpid);
+		}while(tpid != child_pid);
+
+		return child_status;
+	
+	}
 }
 
 int main(int argc, char **argv)
 {
-       int i = 0;
-       int continuer=1;
-       char delims[] = " ";
-       char *result = NULL;
-       char *TousMesMots = malloc(15 * sizeof(char)); // 15 cest nombre de mots
+	int i = 0;
+	int continuer=1;
+	char delims[] = " ";
+	char *result = NULL;
+	char *TousMesMots = malloc(15 * sizeof(char)); // 15 cest nombre de mots
 
- while (continuer){
-               char buffer[tailleBuffer];
+	while (continuer){
+		char buffer[tailleBuffer];
 
-               fgets(buffer,tailleBuffer,stdin);
+		fgets(buffer,tailleBuffer,stdin);
 
-               printf("ma chaine est : %s\n",buffer);
-               
-               result = strtok(buffer, delims );
-               while( result != NULL ) {
-                       printf( "result is \"%s\"\n"", result);
-                       TousMesMots[i] = malloc((strlen(result)) * sizeof(char));
-                       TousMesMots[i] = result; //&result[i]
-                       result = strtok(NULL, delims);
-                       i++;
-               }
-               
-               printf("le troisieme mot est: %s", TousMesMots[2]);
-       }
+		printf("ma chaine est : %s\n",buffer);
 
-       return 0;
+		result = strtok(buffer, delims );
+		while( result != NULL ) {
+			printf( "result is \"%s\"\n"", result);
+			TousMesMots[i] = malloc((strlen(result)) * sizeof(char));
+			TousMesMots[i] = result; //&result[i]
+			result = strtok(NULL, delims);
+			i++;
+		}
+
+		printf("le troisieme mot est: %s", TousMesMots[2]);
+	}
+
+	return 0;
 }

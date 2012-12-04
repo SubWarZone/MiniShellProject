5cc46da8a3cd1e495bf7a0bb3be338fab6934cdd
Deafteen <marcmozgawa@gmail.com>
2012-12-04 14:41:11 +0100

    function compterMots

--- main.c
+++ main.c
@@ -6,17 +6,18 @@
 
 #define tailleBuffer 100
 
-int compterMots(char * chaine){
-	int i=1;
-	int compteur = 0;
-	//nous 
-	while(chaine[i] != '\0'){
-		if(chaine[i] == ' ' && chaine[i-1] != ' '){	
-				compteur++;
+int compterMots(char* chaine, char separateur) {
+	int i = 0; int NbreMots = 0;
+	for(i = 0; i < strlen(chaine)-1; i++) {
+		if((chaine[i] != separateur) && (i == 0)) {
+			NbreMots++;
 		}
-		i++;
+		
+		else if((chaine[i] != separateur) && (chaine[i - 1] == separateur)) {
+			NbreMots++;
+		}  
 	}
-	return compteur + 1; // il y a compteur espace et donc compter+1 mots
+	return NbreMots;
 }
 
 char **separerLaChaine(char *chaine){
@@ -38,6 +39,7 @@ char **separerLaChaine(char *chaine){
 
 int main(int argc, char **argv)
 {
+	int NbreMots = 0;
 	int i = 0;
 	pid_t pid;
 	int continuer=1;
@@ -47,11 +49,16 @@ int main(int argc, char **argv)
 	char **TousMesMots2 = malloc(15 * sizeof(char*));
 	path=getenv("PATH");
 	char delims2[] =":";
+	
+	printf("mon paths : %s\n",path);
+	NbreMots = compterMots(path,':');
+	printf ("nb mots dans le path : %d\n", NbreMots);
+	
 	path =  strtok (path,delims2);
 	
 	//*TEST GRAPHIQUE
 	while( path != NULL ) {
-		printf( "path is \"%s\"\n", path);
+		printf( "path is %s\n", path);
 		TousMesMots2[i] = malloc((strlen(path)) * sizeof(char));
 		TousMesMots2[i] = path; 
 		path = strtok(NULL, delims2);
@@ -65,12 +72,23 @@ int main(int argc, char **argv)
 
 		printf("ma chaine est : %s\n",buffer);
 		
+		NbreMots = compterMots(buffer, ' ');
+		printf("notre chaine contient %d mots\n", NbreMots);
+		
 		char **TousMesMotsP = separerLaChaine(buffer);
 		
 		printf("le premier mot est: %s\n", TousMesMotsP[0]);
 		printf("le deuxieme mot est: %s\n", TousMesMotsP[1]);
 		printf("le troisieme mot est: %s\n", TousMesMotsP[2]);
 		
+/*	if(strcmp(TousMesMotsP[0], "exit ") == 0) {
+			printf("les deux sont egaux\n");
+			exit(1);
+		}
+		else {
+			printf("bite et couille");
+		}
+*/		
 		//char** parametres = {TousMesMots2[1], 
 		/*
 		if ((pid = fork()) == -1)
